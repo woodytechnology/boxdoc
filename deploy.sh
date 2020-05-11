1fe67d04add268e2b42faf7cdbbee0b4557111bd
Kaive Young <kaiveyoung@gmail.com>
2020-05-11 13:49:45 +0800

    vpn

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-gitbook build
-scp -r _book kaive@wuyun.pro:/home/kaive/boxdoc && ssh kaive@wuyun.pro 'sudo rm -rf /usr/share/nginx/html/boxdoc;sudo mv /home/kaive/boxdoc /usr/share/nginx/html/boxdoc'
